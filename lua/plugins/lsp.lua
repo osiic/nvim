a6ed0655ddb74f8e50716a3d4af4c790b5a6d189
Osi ic <osiic.offcl@gmail.com>
2025-06-07 05:35:27 +0700

    hadehh

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -46,17 +46,6 @@ return {
 
       vim.lsp.config("eslint", {
         capabilities = capabilities,
-        on_attach = function(client, bufnr)
-          if not base_on_attach then
-            return
-          end
-
-          base_on_attach(client, bufnr)
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            buffer = bufnr,
-            command = "LspEslintFixAll",
-          })
-        end,
       })
 
       vim.lsp.config("tailwindcss", { capabilities = capabilities })
