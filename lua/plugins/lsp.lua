05f662b2763dd0b5ef1325a3196763e1cc2dc5c7
Osi ic <osiic.offcl@gmail.com>
2025-06-07 04:14:31 +0700

    nice null ls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,23 +3,25 @@ return {
     "mason-org/mason.nvim",
     config = function()
       require("mason").setup()
-    end
+    end,
   },
 
   {
     "mason-org/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "ts_ls"},
+        ensure_installed = { "lua_ls", "ts_ls", "eslint", "tailwindcss" },
         automatic_installation = true,
       })
-    end
+    end,
   },
 
   {
     "neovim/nvim-lspconfig",
     config = function()
-      vim.lsp.config('lua_ls', {
+      local base_on_attach = require("lspconfig").util.default_config.on_attach
+
+      vim.lsp.config("lua_ls", {
         settings = {
           Lua = {
             diagnostics = {
@@ -36,13 +38,13 @@ return {
         },
       })
 
-      vim.lsp.config('ts_ls', {
+      vim.lsp.config("ts_ls", {
         init_options = {
           plugins = {
             {
               name = "@vue/typescript-plugin",
               location = "/usr/local/lib/node_modules/@vue/typescript-plugin",
-              languages = {"javascript", "typescript", "vue"},
+              languages = { "javascript", "typescript", "vue" },
             },
           },
         },
@@ -53,7 +55,21 @@ return {
         },
       })
 
-    end
-  },
+      vim.lsp.config("eslint", {
+        on_attach = function(client, bufnr)
+          if not base_on_attach then
+            return
+          end
 
+          base_on_attach(client, bufnr)
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            buffer = bufnr,
+            command = "LspEslintFixAll",
+          })
+        end,
+      })
+
+      vim.lsp.enable("tailwindcss")
+    end,
+  },
 }
