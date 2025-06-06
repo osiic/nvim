851a6b25a312aed1a0a9e9876ee3f7f139efab72
Osi ic <osiic.offcl@gmail.com>
2025-06-07 05:33:02 +0700

    benerin ts ls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -42,20 +42,6 @@ return {
 
       vim.lsp.config("ts_ls", {
         capabilities = capabilities,
-        init_options = {
-          plugins = {
-            {
-              name = "@vue/typescript-plugin",
-              location = "/usr/local/lib/node_modules/@vue/typescript-plugin",
-              languages = { "javascript", "typescript", "vue" },
-            },
-          },
-        },
-        filetypes = {
-          "javascript",
-          "typescript",
-          "vue",
-        },
       })
 
       vim.lsp.config("eslint", {
