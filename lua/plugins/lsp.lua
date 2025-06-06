568e847e894fae5a65e9ad88852f31217009cbcc
Osi ic <osiic.offcl@gmail.com>
2025-06-07 02:42:50 +0700

    benerin lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,138 +1,59 @@
--- plugins/lsp.lua
 return {
-	{
-		"williamboman/mason.nvim",
-		cmd = "Mason",
-		build = ":MasonUpdate",
-		opts = {
-			ui = {
-				border = "rounded",
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗",
-				},
-			},
-		},
-	},
+  {
+    "mason-org/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
 
-	{
-		"williamboman/mason-lspconfig.nvim",
-		dependencies = { "williamboman/mason.nvim" },
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"ts_ls",
-					"html",
-					"cssls",
-					"tailwindcss",
-					"intelephense",
-					"emmet_ls",
-					"lua_ls",
-					"jsonls",
-					"yamlls",
-				},
-				automatic_installation = true,
-			})
-		end,
-	},
+  {
+    "mason-org/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "ts_ls"},
+        automatic_installation = true,
+      })
+    end
+  },
 
-	{
-		"neovim/nvim-lspconfig",
-		event = { "BufReadPre", "BufNewFile" },
-		dependencies = {
-			"hrsh7th/cmp-nvim-lsp",
-			"ray-x/lsp_signature.nvim",
-		},
-		config = function()
-			local lspconfig = require("lspconfig")
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local on_attach = require("core.lsp").on_attach
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      vim.lsp.config('lua_ls', {
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" },
+            },
+            workspace = {
+              library = vim.api.nvim_get_runtime_file("", true),
+              checkThirdParty = false,
+            },
+            telemetry = {
+              enable = false,
+            },
+          },
+        },
+      })
 
-			-- Common LSP servers
-			local servers = {
-				"ts_ls",
-				"html",
-				"cssls",
-				"tailwindcss",
-				"intelephense",
-				"emmet_ls",
-				"jsonls",
-				"yamlls",
-			}
+      vim.lsp.config('ts_ls', {
+        init_options = {
+          plugins = {
+            {
+              name = "@vue/typescript-plugin",
+              location = "/usr/local/lib/node_modules/@vue/typescript-plugin",
+              languages = {"javascript", "typescript", "vue"},
+            },
+          },
+        },
+        filetypes = {
+          "javascript",
+          "typescript",
+          "vue",
+        },
+      })
 
-			for _, server in ipairs(servers) do
-				lspconfig[server].setup({
-					capabilities = capabilities,
-					on_attach = on_attach,
-				})
-			end
+    end
+  },
 
-			-- Special configuration for Lua
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-				on_attach = on_attach,
-				settings = {
-					Lua = {
-						runtime = { version = "LuaJIT" },
-						diagnostics = { globals = { "vim" } },
-						workspace = {
-							checkThirdParty = false,
-							library = vim.api.nvim_get_runtime_file("", true),
-						},
-						telemetry = { enable = false },
-					},
-				},
-			})
-
-			-- Enhanced LSP UI
-			require("lsp_signature").setup({
-				bind = true,
-				handler_opts = { border = "rounded" },
-				hint_enable = false,
-			})
-		end,
-	},
-
-	{
-		"stevearc/conform.nvim",
-		event = { "BufWritePre" },
-		config = function()
-			require("conform").setup({
-				formatters_by_ft = {
-					javascript = { "prettier" },
-					typescript = { "prettier" },
-					javascriptreact = { "prettier" },
-					typescriptreact = { "prettier" },
-					php = { "php_cs_fixer" },
-					lua = { "stylua" },
-					json = { "prettier" },
-					yaml = { "prettier" },
-					html = { "prettier" },
-					css = { "prettier" },
-					markdown = { "prettier" },
-				},
-				format_on_save = {
-					timeout_ms = 1500,
-					lsp_fallback = true,
-				},
-			})
-		end,
-	},
-
-	{
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-		dependencies = { "williamboman/mason.nvim" },
-		config = function()
-			require("mason-tool-installer").setup({
-				ensure_installed = {
-					"prettier",
-					"stylua",
-					"php-cs-fixer",
-					"eslint_d",
-					"typescript-language-server",
-				},
-			})
-		end,
-	},
 }
