4862e653cbae6b367399287c7d97e4884c6019a8
Osi ic <osiic.offcl@gmail.com>
2025-08-20 00:16:09 +0700

    Update options.lua

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -47,3 +47,10 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.hl.on_yank()
 	end,
 })
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+  pattern = "*",
+  callback = function()
+    vim.cmd([[ %s/\r//g ]])
+  end,
+})
