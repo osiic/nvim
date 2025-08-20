646d78618b41f5362d94cf88b9cd03dc82bde6ed
Osi ic <osiic.offcl@gmail.com>
2025-08-20 14:58:01 +0700

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Osiic's Neovim config";
+
+  outputs = { self, nixpkgs }: {
+    # Module khusus home-manager
+    homeManagerModules.nvim = { config, pkgs, lib, ... }: {
+      programs.neovim = {
+        enable = true;
+        viAlias = true;
+        vimAlias = true;
+
+        # Bisa tambahin plugin langsung dari nixpkgs juga
+        plugins = with pkgs.vimPlugins; [
+          vim-nix
+          nil
+          telescope-nvim
+          nvim-treesitter
+          nixpkgs-fmt
+        ];
+
+        # Ambil langsung init.lua / init.vim dari repo ini
+        extraLuaConfig = builtins.readFile ./.config/nvim/init.lua;
+      };
+    };
+  };
+}
